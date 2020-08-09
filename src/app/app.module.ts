3baa40d34d081137f590ce3923e73a0154f00080
Felipe <felipee.b95@gmail.com>
2020-08-09 18:33:43 -0400

    First commit
    * Program without NgRX

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HijoComponent,
+    NietoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
